f5da17ef4d5e8a54d0172b39b828b3f262df3d41
Jace <jacehensley@gmail.com>
2019-11-07 00:16:15 -0700

    Prep release of 1.1.2

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,27 @@
 import React from 'react'
 import {O} from 'ts-toolbelt'
 
-export type ComponentProps<K extends keyof JSX.IntrinsicElements, P extends object = {}> = O.Diff<JSX.IntrinsicElements[K], P> & P
+export type ComponentProps<K extends keyof JSX.IntrinsicElements, P extends {[key: string]: any} = {}> = O.Diff<
+  JSX.IntrinsicElements[K],
+  P
+> &
+  P
 
-export type FC<K extends keyof JSX.IntrinsicElements, P extends object = {}> = React.FC<ComponentProps<K, P>>
+export type FC<K extends keyof JSX.IntrinsicElements, P extends {[key: string]: any} = {}> = React.FC<ComponentProps<K, P>>
 
-export const forwardRef = <K extends keyof JSX.IntrinsicElements, P extends object = {}, T = any>(
+export const forwardRef = <K extends keyof JSX.IntrinsicElements, P extends {[key: string]: any} = {}, T = any>(
   component: React.RefForwardingComponent<T, ComponentProps<K, P>>,
 ) => React.forwardRef<T, ComponentProps<K, P>>(component)
 
-export class Component<K extends keyof JSX.IntrinsicElements, P extends object = {}, S = {}> extends React.Component<
+export class Component<K extends keyof JSX.IntrinsicElements, P extends {[key: string]: any} = {}, S = {}> extends React.Component<
   ComponentProps<K, P>,
   S
 > {}
 
-export const forwardProps = <P extends {[key: string]: any}, S extends string>(props: any, ...rm: S[]): Omit<P, S> => {
+export const forwardProps = <P extends {[key: string]: any}, S extends string = ''>(props: P, ...rm: S[]): Omit<P, S> => {
+  if (!rm || rm.length === 0) return {...props}
   const forwardedProps = {} as P
-  const rmObj = {} as {[key in S]: boolean}
+  const rmObj: {[key in S]?: true} = {}
   rm.forEach(key => {
     rmObj[key] = true
   })
